734c7719967404415f66cd976a7c4748141d98b5
Sanjit Dutta <sdlynx@gmail.com>
2020-10-31 17:18:45 -0400

    Pandora changed what info is conveyed, this seems to work for speaker but not chromecasts yet

--- apps/mute_ads.py
+++ apps/mute_ads.py
@@ -4,13 +4,13 @@ class mute_ads(hass.Hass):
 
     def initialize(self):
         for media_player in self.args["media_players"]:
-            self.listen_state(self.handle_mute, media_player, attribute = "media_album_name")
+            self.listen_state(self.handle_mute, media_player, attribute = "media_title")
 
     def handle_mute(self, entity, attribute, old, new, kwargs):
-        self.log("old media_album_name: %s, new media_album_name: %s" % (old, new))
+        self.log("old media_title: %s, new media_title: %s" % (old, new))
         if new != old:
-            if new == "Advertisement":
+            if new == "":
                 self.call_service("media_player/volume_mute", entity_id=entity, is_volume_muted=True)
-            if old == "Advertisement":
+            if old == "":
                 self.call_service("media_player/volume_mute", entity_id=entity, is_volume_muted=False)
 
