ac19586dd3f67ca5fcf7c02a9cf2a33af9bd106e
Sanjit Dutta <sdlynx@gmail.com>
2020-10-31 17:19:03 -0400

    change volume to same level for all playing devices

--- apps/control_volume.py
+++ apps/control_volume.py
@@ -3,23 +3,50 @@ import appdaemon.plugins.hass.hassapi as hass
 class control_volume(hass.Hass):
 
     def initialize(self):
+
       for (i, source) in enumerate(self.args["volume_up"]["trigger_sources"]):
+
         kwargs = {}
         kwargs[self.args["volume_up"]["event_data_keys"][i]] = self.args["volume_up"]["event_data_values"][i]
+
         self.listen_event(self.change_volume, event=source, increase_volume=False, **kwargs)
+
       for (j, source) in enumerate(self.args["volume_down"]["trigger_sources"]):
+
         kwargs = {}
         kwargs[self.args["volume_down"]["event_data_keys"][j]] = self.args["volume_down"]["event_data_values"][j]
+
         self.listen_event(self.change_volume, event=source, increase_volume=True, **kwargs)
 
     def change_volume(self, event, attributes, kwargs):
-      for media_player in self.args["media_players"]:
-          is_muted = self.get_state(entity_id=media_player, attribute="is_volume_muted")
 
-          # something is playing
-          if is_muted == False:
-            current_volume = self.get_state(entity_id=media_player, attribute="volume_level")
-            if kwargs["increase_volume"]:
-              self.call_service("media_player/volume_up", entity_id=media_player)
-            else:
-              self.call_service("media_player/volume_down", entity_id=media_player)
+      max_volume = 0
+
+      for media_player in self.args["media_players"]:
+
+        # something is playing
+        is_muted = self.get_state(entity_id=media_player, attribute="is_volume_muted")
+        if is_muted == False:
+
+          current_volume = self.get_state(entity_id=media_player, attribute="volume_level")
+          max_volume = max(current_volume, max_volume)
+
+          # self.log("max volume %f", max_volume)
+
+      for media_player in self.args["media_players"]:
+
+        # something is playing
+        is_muted = self.get_state(entity_id=media_player, attribute="is_volume_muted")
+        if is_muted == False:
+
+          if kwargs["increase_volume"]:
+
+            set_volume = min(1, max_volume + .1)
+            self.call_service("media_player/volume_set", entity_id=media_player, volume_level=set_volume)
+
+          else:
+
+            set_volume = max(0, max_volume - .1)
+            self.call_service("media_player/volume_set", entity_id=media_player, volume_level=set_volume)
+
+          # self.log("set volume %f", set_volume)
