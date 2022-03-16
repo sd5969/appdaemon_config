d546a9e6ba9d8508be1aeab3d9d1eba8747538b2
Sanjit Dutta <sdlynx@gmail.com>
2022-03-16 14:28:25 +0000

    detect_noise uses frontend threshold

--- apps/detect_noise.py
+++ apps/detect_noise.py
@@ -32,7 +32,7 @@ class detect_noise(hass.Hass):
           current_volume = self.get_state(entity_id=media_player, attribute="volume_level")
           
           # self.log("%s", new == "True")
-          if new == "True": # this is a string???
+          if new == "True": # this is a string?? anyway, this is the sensor saying noise was detected
 
             set_volume = min(1, current_volume + GAIN)
             self.call_service("media_player/volume_set", entity_id=media_player, volume_level=set_volume)
